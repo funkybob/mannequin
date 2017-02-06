1e399f96187e88ee0659b74fa1087feff25b1197
Curtis Maloney <curtis@tinbrain.net>
2017-02-06 19:38:48 +1100

    All values are strings

--- mannequin/stores/dynamodb.py
+++ mannequin/stores/dynamodb.py
@@ -1,6 +1,6 @@
 import botocore.session
 
-from .. import attr
+from .. import attrs
 from .base import BaseStore
 
 
@@ -10,6 +10,7 @@ session = botocore.session.get_session()
 class DynamoStore(BaseStore):
     def __init__(self, model, hash_key=None, range_key=None, table=None,
                  client=None):
+        super(DynamoStore, self).__init__(model)
         if client is None:
             client = session.create_client('dynamodb')
         self.client = client
@@ -18,10 +19,10 @@ class DynamoStore(BaseStore):
         self.table = table
         # XXX Validate these keys exist in the model fields!
         self.hash_key = hash_key
-        assert hash_key in self.fields
+        assert hash_key in self.model._fields
         self.range_key = range_key
         if range_key is not None:
-            assert range_key in self.fields
+            assert range_key in self.model._fields
 
     def get(self, hash_key, range_key=None):
         if self.range_key and range_key is None:
@@ -48,7 +49,7 @@ class DynamoStore(BaseStore):
             TableName=self.table,
             Item={
                 field: self.value_for_field(field, getattr(model, field))
-                for field in self.fields
+                for field in self.model._fields
             }
         )
 
@@ -61,9 +62,9 @@ class DynamoStore(BaseStore):
         return key
 
     def value_for_field(self, field_name, value):
-        field = self.fields[field_name]
-        if isinstance(field, (attr.IntAttr, attr.NumberAttr)):
-            return {'N': value}
-        if isinstance(field, attr.BoolAttr):
+        field = self.model._fields[field_name]
+        if isinstance(field, (attrs.IntAttr, attrs.NumberAttr)):
+            return {'N': str(value)}
+        if isinstance(field, attrs.BoolAttr):
             return {'BOOL': str(value)}
         return {'S': value}
