93b9064cd8ef7ba20591950a7c205510136e4872
Curtis Maloney <curtis@tinbrain.net>
2017-02-06 19:38:29 +1100

    Model._fields is now done in model

--- mannequin/stores/base.py
+++ mannequin/stores/base.py
@@ -1,5 +1,3 @@
-from inspect import classify_class_attrs
-from ..attr import Attr
 
 
 class BaseStore(object):
@@ -12,17 +10,6 @@ class BaseStore(object):
         Configure a new Store inteface for the given model class.
         '''
         self.model = model
-        self.fields = self.get_model_fields(model)
-
-    def get_model_fields(self):
-        '''
-        Return a map of name : attr for all fields on this model.
-        '''
-        return {
-            name: prop
-            for name, kind, cls, prop in classify_class_attrs(self.model)
-            if isinstance(prop, Attr)
-        }
 
     def get(self, *args, **kwargs):
         raise NotImplementedError
