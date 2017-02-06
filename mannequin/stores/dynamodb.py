180c1bdb48d64ca775b8af7b1e53302039b2b90c
Curtis Maloney <curtis@tinbrain.net>
2017-02-06 20:36:59 +1100

    Add create_table method

--- mannequin/stores/dynamodb.py
+++ mannequin/stores/dynamodb.py
@@ -4,14 +4,12 @@ from .. import attrs
 from .base import BaseStore
 
 
-session = botocore.session.get_session()
-
-
 class DynamoStore(BaseStore):
     def __init__(self, model, hash_key=None, range_key=None, table=None,
                  client=None):
         super(DynamoStore, self).__init__(model)
         if client is None:
+            session = botocore.session.get_session()
             client = session.create_client('dynamodb')
         self.client = client
         if table is None:
@@ -62,9 +60,51 @@ class DynamoStore(BaseStore):
         return key
 
     def value_for_field(self, field_name, value):
+        return {self.type_for_field(field_name): str(value)}
+
+    def type_for_field(self, field_name):
         field = self.model._fields[field_name]
         if isinstance(field, (attrs.IntAttr, attrs.NumberAttr)):
-            return {'N': str(value)}
+            return 'N'
         if isinstance(field, attrs.BoolAttr):
-            return {'BOOL': str(value)}
-        return {'S': value}
+            return 'BOOL'
+        return 'S'
+
+    def create_table(self):
+        '''
+        '''
+        key_schema = [
+            {
+                'AttributeName': self.hash_key,
+                'KeyType': 'HASH',
+            },
+        ]
+        attr_defs = [
+            {
+                'AttributeName': self.hash_key,
+                'AttributeType': self.type_for_field(self.hash_key),
+            }
+        ]
+        if self.range_key:
+            key_schema.append(
+                {
+                    'AttributeName': self.range_key,
+                    'KeyType': 'RANGE',
+                }
+            )
+            attr_defs.append(
+                {
+                    'AttributeName': self.range_key,
+                    'AttributeType': self.type_for_field(self.range_key),
+                }
+            )
+
+        self.client.create_table(
+            TableName=self.table,
+            AttributeDefinitions=attr_defs,
+            KeySchema=key_schema,
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5,
+            },
+        )
