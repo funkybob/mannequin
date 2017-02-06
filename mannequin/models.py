349c04881a6c0ffbb442c72494b73e3c96f303a0
Curtis Maloney <curtis@tinbrain.net>
2017-02-06 19:37:29 +1100

    Add metaclass to Model to set Attr names

--- mannequin/models.py
+++ mannequin/models.py
@@ -1,7 +1,27 @@
+from inspect import classify_class_attrs
+
+from .attrs import Attr
+
+
+class MetaModel(type):
+    def __new__(mcs, name, bases, attrs):
+        klass = super(MetaModel, mcs).__new__(mcs, name, bases, attrs)
+
+        klass._fields = {
+            name: prop
+            for name, kind, cls, prop in classify_class_attrs(klass)
+            if isinstance(prop, Attr)
+        }
+
+        for field in klass._fields:
+            getattr(klass, field).name = field
+        return klass
 
 
 class Model(object):
+    __metaclass__ = MetaModel
 
     def __init__(self, **kwargs):
+        self._data = {}
         for key, value in kwargs.items():
             setattr(self, key, value)
