dcc4d8e2ee9ccd596621a118dbe8aeb85a889c27
Curtis Maloney <curtis@tinbrain.net>
2017-02-06 20:37:07 +1100

    Make tests work+pass

--- tests/test_dynamodb.py
+++ tests/test_dynamodb.py
@@ -1,9 +1,14 @@
 import unittest
 
+import botocore
 
 from mannequin import models
 from mannequin import attrs
 from mannequin.stores import dynamodb
+
+
+session = botocore.session.get_session()
+client = session.create_client('dynamodb', endpoint_url='http://localhost:9000')
 
 
 class SimpleModel(models.Model):
@@ -11,14 +16,20 @@ class SimpleModel(models.Model):
     age = attrs.IntAttr()
 
 
-SimpleStore = dynamodb.DynamoStore(SimpleModel, hash_key='name')
+SimpleStore = dynamodb.DynamoStore(SimpleModel, table="simple",
+                                   hash_key='name', client=client)
 
 
 class DynamoTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # SimpleStore.create_table()
+        pass
+
     def test_simple(self):
 
         obj = SimpleModel(name='Bob', age=42)
         SimpleStore.save(obj)
 
-        obj2 = SimpleStore.get(key='Bob')
+        obj2 = SimpleStore.get('Bob')
         self.assertEqual(obj2.age, 42)
