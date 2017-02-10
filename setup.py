59943f210171435d47f76e1d200e20a93a8bfb42
Curtis Maloney <curtis@tinbrain.net>
2017-02-10 16:12:21 +1100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='Mannequin',
+    version='0.0.1',
+    description='Storage agnostic data models',
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    url='https://github.com/funkybob/mannequin',
+    packages=['mannequin', 'mannequin.stores'],
+)
